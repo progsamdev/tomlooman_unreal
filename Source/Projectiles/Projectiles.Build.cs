b78c30e18acf1d46e055f26b8645fb595381c9a5
progsamdev <samuel.msbr@gmail.com>
2026-02-21 16:49:31 -0300

    fix: reset branches 2

--- Source/Projectiles/Projectiles.Build.cs
+++
@@ -1,9 +0,0 @@
-using UnrealBuildTool;
-
-public class Projectiles: ModuleRules
-{
-    public Projectiles(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine"});
-    }
-}
